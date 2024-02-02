4790e0d56631f8e114e7b34444561884d79b7b9f
Josh Stockdale <joshdstockdale@gmail.com>
2024-02-02 14:59:22 -0500

    add mongo and basic error handling

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/joshdstockdale/go-booking/api"
-	"github.com/joshdstockdale/go-booking/types"
+	"github.com/joshdstockdale/go-booking/db"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
@@ -16,30 +16,28 @@ const dburi = "mongodb://localhost:27017"
 const dbname = "hotel-reservation"
 const userColl = "users"
 
+var config = fiber.Config{
+	ErrorHandler: func(ctx *fiber.Ctx, err error) error {
+		return ctx.JSON(map[string]string{"error": err.Error()})
+	},
+}
+
 func main() {
-	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(dburi))
-	if err != nil {
-		log.Fatal(err)
-	}
-	ctx := context.Background()
-	coll := client.Database(dbname).Collection(userColl)
-	user := types.User{
-		FirstName: "Josh",
-		LastName:  "Stock",
-	}
-	res, err := coll.InsertOne(ctx, user)
-	if err != nil {
-		log.Fatal(err)
-	}
-	log.Println(res)
 
 	listenAddr := flag.String("listenAddr", ":5000", "Listen address of the API Server")
 	flag.Parse()
 
-	app := fiber.New()
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(dburi))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	userHandler := api.NewUserHandler(db.NewMongoUserStore(client))
+
+	app := fiber.New(config)
 	apiv1 := app.Group("/api/v1")
 
-	apiv1.Get("/user", api.HandleGetUsers)
-	apiv1.Get("/user/:id", api.HandleGetUser)
+	apiv1.Get("/user", userHandler.HandleGetUsers)
+	apiv1.Get("/user/:id", userHandler.HandleGetUser)
 	app.Listen(*listenAddr)
 }
